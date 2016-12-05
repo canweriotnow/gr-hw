20510d53ea23a67416ae2ab094f05a5c3590d929
Jason Lewis <jason@decomplecting.org>
2016-12-05 05:11:15 -0500

    Update README.org and project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject gr-hw "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Guaranteed Rate Interview Homework"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [metosin/compojure-api "1.1.8"]
                  [ring "1.5.0"]
@@ -7,7 +7,6 @@
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.csv "0.1.3"]
                  [semantic-csv "0.1.0"]
-                 [org.clojure/java.jdbc "0.7.0-alpha1"]
                  [com.stuartsierra/component "0.3.1"]
                  [org.danielsz/system "0.3.1"]
                  [http-kit "2.2.0"]
